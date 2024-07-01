b50533dd06942c3eb1611eefcbd7650a7ff4485c
William Jolivet <william.jolivet@epitech.eu>
2024-07-01 17:05:58 +0200

    [ADD] game grid

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,1 +1,27 @@
-<h1>Hello World!</h1>
+<script>
+    let board = Array(3).fill("").map(() => Array(3).fill(""));
+
+    function handleClick(row, col) {
+        console.log(`Clicked on row ${row} and col ${col}`);
+    }
+</script>
+
+<style>
+    .board {
+        display: grid;
+        grid-template-columns: repeat(3, 0fr);
+        gap: 10px;
+    }
+    button {
+        height: 150px;
+        width: 150px;
+    }
+</style>
+
+<div class="board">
+    {#each board as row, i}
+        {#each row as cell, j}
+            <button on:click={() => handleClick(i, j)}>{cell}</button>
+        {/each}
+    {/each}
+</div>
