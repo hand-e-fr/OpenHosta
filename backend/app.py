efc3be5cd4892d185e67cbbb865791aa2b9d03a1
William Jolivet <william.jolivet@epitech.eu>
2024-07-02 10:36:56 +0200

    [ADD] add frontend visuals

--- backend/app.py
+++ backend/app.py
@@ -21,45 +21,9 @@ def hello_world():
     return say_hello(random.choice(string.ascii_uppercase))
 
 
-@llm.emulate
-def emulate_tic_tac_toe(board: str) -> str:
-    """
-    This function emulates a tic-tac-toe game.
-    You must provide the current state of the board as a list of lists.
-    Each list represents a row of the board.
-    Each element of the list represents a cell of the board.
-    The value of each cell can be "-", "X" or "O".
-    The function returns the next state of the board and the status of the game.
-    The status can be "draw" or "win".
-    example of received board:
-    ```json
-    {
-        "board": [
-            ["-", "-", "-"],
-            ["-", "X", "-"],
-            ["-", "-", "-"]
-        ]
-    }
-    ```
-    example of returned board:
-    draw:
-    ```json
-    {
-        "board": [
-            ["-", "O", "-"],
-            ["-", "X", "-"],
-            ["-", "-", "-"]
-        ],
-        "status": "draw"
-    }
-    ```
-    """
-    pass
-
-
 @app.route('/tic-tac-toe', methods=['POST'])
 def tic_tac_toe():
-    return emulate_tic_tac_toe()
+
 
 
 if __name__ == '__main__':
