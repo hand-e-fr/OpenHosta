5ff6303f5f1af4b69ce8872c938c0b081e532b13
khyk <merlin.devillard@epitech.eu>
2024-07-03 18:05:00 +0200

    [ADD] enhancer request

--- enhancer.py
+++ enhancer.py
@@ -1,44 +1,42 @@
-import inspect
 import requests
 import json
-import openai
+import sys
 
 enhancer_pre_prompt:str = """
 I want you to become my Expert Prompt Creator for developer.
 Your goal is to help me craft the best possible prompt for my programming needs.
 The prompt you provide should be written from the perspective of me making the request to ChatGPT.
 Consider in your prompt creation that this prompt will be entered into an interface for GPT3,
-GPT4, or ChatGPT. The prompt will include instructions to write the output using my communication
-style. The process is as follows:
+GPT4, or ChatGPT. The process is as follows:
 
 You will generate the following sections:
 
 "
-**Prompt:**
+-> Prompt:
 {provide the best possible prompt according to my request. The prompt is used to describe
 a function to be performed in Python as precisely as possible. You can add error handling,
 as the function needs it to work properly.}
 
-**Critique:**
+-> Critique:
 {provide a concise paragraph on how to improve the prompt. Be very critical in your response.
 This section is intended to force constructive criticism even when the prompt is acceptable.
 Any assumptions and or issues should be included. Don't forget that you speak to a devloper}
 
-**Proposition:**
+-> Proposition:
 {rewrite the prompt with the suggested improvements in the critique category.
 The aim is to make a proposal, an example. Make logical assumptions to solve
 the problem based on the context and all the information you have.}
 
-**Diagram:**
+-> Diagram:
 {Make a mermaid diagram explaining the function described by the prompt. You need to break
 the reasoning into several steps for ease of understanding and clarity. You must
 make it logical and easy to look at. You have to write it in mermaid syntax.}
 "
-
-Here's my python function's prototype:
-
-Here's my python function's prompt:
 """
+# se documenter sur le rôle assistant
+# créer la fonction de parsing 
+# créer la fonction de retour 
+# créer la visualtion de diagramme et l'expérience utilisateur
 
 class enhancer:
     __last_prompt__:str = None
@@ -54,16 +52,52 @@ class enhancer:
     def __init__(self, api_key:str=None, model:str=None)->None:
         self.api_key = enhancer._default_api_key if api_key == None else api_key
         self.model = enhancer._default_ai_model if model == None else model
-        self.temperature = 0.7
-        self.top_d = 0.7
+        self.temperature = 0.5
+        self.top_d = 0.5
+        
+    def ai_call_request(self, sys_prompt:str, func_prot:str, func_doc:str):
+        api_key = self.api_key
+        url = 'https://api.openai.com/v1/chat/completions'
 
-    def ai_call(self, prompt:str)->dict:
-        openai.api_key = self.api_key
-        try:
-            response = openai.completions.create(
-                model = self.model
-                prompt = prompt
-                temperature = self.temperature
-                top_p = self.top_p
-            )
-        except:
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {api_key}',
+        }
+
+        data = {
+            "model": "gpt-4o",
+            "messages": [
+                {
+                "role": "system",
+                "content": [{
+                    "type": "text",
+                    "text": sys_prompt
+                }]},{
+                "role": "user",
+                "content": [{
+                    "type": "text",
+                    "text": "\nHere's my python function's prototype:\n---\n" + func_prot + "\n---\n"
+                }]},{
+                "role": "user",
+                "content": [{
+                    "type": "text",
+                    "text": "\nHere's my python function's prompt:\n---\n" + func_doc + "\n---\n"
+                }]},
+            ],
+            "temperature": 0.5,
+            "top_p": 0.5
+        }
+        
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+
+        if response.status_code == 200:
+            response_data = response.json()
+            print(response_data['choices'][0]['message']['content'])
+        else:
+            sys.stderr.write("[CALL_ERROR] The request was unsuccessful or one of the parameters is invalid")
+            sys.stderr.write(f"Status: {response.status_code}")
+            print(response.json())
+                
+en = enhancer(None, None)
+
+en.ai_call_request(enhancer_pre_prompt, "def compute(a:int, b:int)->int:", "The function sums the two numbers and multiplies the difference")
