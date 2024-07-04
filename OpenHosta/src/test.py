51bd6fd3617d1372934e31b66b18966d94f5da9a
khyk <merlin.devillard@epitech.eu>
2024-07-04 16:02:38 +0200

    [ADD] file maker

---
+++ OpenHosta/src/test.py
@@ -0,0 +1,11 @@
+import emulator as emu
+import enhancer as enh
+
+@enh.enhancer().enhance
+def compute(a:int, b:int)->int:
+    """
+    The function sums the two numbers and multiplies the difference
+    """
+    pass
+
+compute(2, 3)
