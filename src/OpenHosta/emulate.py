327e7aadf06fde6748487ffb12516de9428e56c5
khyk <merlin.devillard@epitech.eu>
2024-08-29 17:48:50 +0200

    [FIX] code formatting

--- src/OpenHosta/emulate.py
+++ src/OpenHosta/emulate.py
@@ -8,49 +8,48 @@ _x = PromptMananger()
 
 _emulator_pre_prompt = _x.get_prompt("emulate")
 
+
 @request_timer
 def _exec_emulate(
     _function_doc=None,
     _function_call=None,
     _function_return=None,
-    model:Model = _default_model,
+    model: Model = _default_model,
     warn: bool = False,
     l_creativity: float = None,
     l_diversity: float = None,
 ):
     global _emulator_pre_prompt
-    
+
     try:
         if not isinstance(_emulator_pre_prompt, str) or not _emulator_pre_prompt:
             raise ValueError("Invalid prompt.")
         if (l_creativity is not None and (l_creativity < 0 or l_creativity > 1)) or (
             l_diversity is not None and (l_diversity < 0 or l_diversity > 1)
         ):
-            raise ValueError(
-                "Emulate out of range values (0<creativity|diversity<1)"
-            )
+            raise ValueError("Emulate out of range values (0<creativity|diversity<1)")
     except ValueError as v:
         sys.stderr.write(f"[EMULATE_ERROR]: {v}")
         return None
-    
+
     l_user_prompt = (
-        "Here's the function definition:\n" 
-        + _function_doc 
-        + "\nAnd this is the function call:\n" 
+        "Here's the function definition:\n"
+        + _function_doc
+        + "\nAnd this is the function call:\n"
         + _function_call
-        +  "\nThis need to return the following JSON format\n"
+        + "\nThis need to return the following JSON format\n"
         + str(_function_return)
     )
-    
+
     response = model._api_call(
-         sys_prompt=_emulator_pre_prompt,
-         user_prompt=l_user_prompt, 
-         creativity=l_creativity, 
-         diversity=l_diversity
+        sys_prompt=_emulator_pre_prompt,
+        user_prompt=l_user_prompt,
+        creativity=l_creativity,
+        diversity=l_diversity,
     )
-    
+
     l_ret = ""
-    
+
     if response.status_code == 200:
         l_ret = model._request_handler(response)
     else:
--- src/OpenHosta/exec.py
+++ src/OpenHosta/exec.py
@@ -5,31 +5,34 @@ from pydantic import BaseModel, create_model
 
 from enhancer import enhance
 
+
 class HostaInjector:
 
     def __init__(self, exec):
         if not callable(exec):
             raise TypeError("Executive function must be a function.")
         self.exec = exec
-    
+
     def __call__(self, *args, **kwargs):
         infos = {"def": "", "call": "", "return_type": ""}
-        
+
         func_obj, caller = self._extend_scope()
         infos["def"], func_prot = self._get_functionDef(func_obj)
         infos["call"] = self._get_functionCall(func_obj, caller)
         infos["return_type"] = self._get_functionReturnType(func_obj)
-    
-        self._attach_attributs(func_obj, func_prot)        
-        return self.exec(infos["def"], infos["call"], infos["return_type"], *args, **kwargs)
-        
-    def _extend_scope(self)->Callable:
+
+        self._attach_attributs(func_obj, func_prot)
+        return self.exec(
+            infos["def"], infos["call"], infos["return_type"], *args, **kwargs
+        )
+
+    def _extend_scope(self) -> Callable:
         try:
             x = inspect.currentframe()
             caller = x.f_back.f_back
             name = caller.f_code.co_name
             func = caller.f_globals.get(name)
-            
+
             if func is None:
                 raise Exception("Scope can't be extend.")
             if not callable(func):
@@ -38,8 +41,8 @@ class HostaInjector:
             sys.stderr.write(f"[FRAME_ERROR]: {e}")
             return None
         return func, caller
-    
-    def _get_functionDef(self, func:Callable)->str:
+
+    def _get_functionDef(self, func: Callable) -> str:
         sig = inspect.signature(func)
 
         func_name = func.__name__
@@ -61,24 +64,24 @@ class HostaInjector:
         definition = f"def {func_name}({func_params}):{func_return}\n    '''\n    {func.__doc__}\n    '''"
         prototype = f"def {func_name}({func_params}):{func_return}"
         return definition, prototype
-    
-    def _get_functionCall(self, func:Callable, caller)->str:
+
+    def _get_functionCall(self, func: Callable, caller) -> str:
         args, _, _, values = inspect.getargvalues(caller)
-        
+
         sig = inspect.signature(func)
         bound_args = {}
         bound_args = sig.bind_partial(**values)
         bound_args.apply_defaults()
-        
+
         args_str = ", ".join(
             f"{name}={value!r}" if name in bound_args.kwargs else f"{value!r}"
             for name, value in bound_args.arguments.items()
         )
-        
+
         call = f"{func.__name__}({args_str})"
         return call
-    
-    def _inspect_returnType(self, func:Callable)->str:
+
+    def _inspect_returnType(self, func: Callable) -> str:
         sig = inspect.signature(func)
 
         if sig.return_annotation != inspect.Signature.empty:
@@ -94,15 +97,18 @@ class HostaInjector:
             if issubclass(return_type, BaseModel):
                 return_json = return_type.model_json_schema()
             else:
-                new_model = create_model('Hosta_return_specified', return_hosta_type=(return_type, ...))
+                new_model = create_model(
+                    "Hosta_return_specified", return_hosta_type=(return_type, ...)
+                )
                 return_json = new_model.model_json_schema()
         else:
-            No_return_specified = create_model('Hosta_return_no_specified', return_hosta_type=(Any, ...))
+            No_return_specified = create_model(
+                "Hosta_return_no_specified", return_hosta_type=(Any, ...)
+            )
             return_json = No_return_specified.model_json_schema()
-        
+
         return return_json
-    
-    def _attach_attributs(self, func: Callable, prototype:str):
+
+    def _attach_attributs(self, func: Callable, prototype: str):
         func.__suggest__ = enhance
         func._prot = prototype
-        
