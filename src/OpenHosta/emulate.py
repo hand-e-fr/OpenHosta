b369898fc8815da27faa8c26a4414ec96efb9535
Khyk <146129739+MerlinDEVILLARD@users.noreply.github.com>
2024-09-06 10:58:11 +0200

    [ADD] Class Method compatibility & Locals variable handler. Closes #17

--- src/OpenHosta/emulate.py
+++ src/OpenHosta/emulate.py
@@ -25,6 +25,7 @@ def _exec_emulate(
     _function_doc = _function_infos["function_def"]
     _function_call = _function_infos["function_call"]
     _function_return = _function_infos["return_type"]
+    _function_locals = _function_infos["function_locals"]
 
     if model is None:
         model = DefaultManager.get_default_model()
@@ -53,7 +54,14 @@ def _exec_emulate(
             + "\nTo fill the “return” value in the output JSON, build your response as defined in the following JSON schema. Do not change the key \"return\"\n"
             + str(_function_return)
         )
-
+        
+    if _function_locals != {}:
+        l_user_prompt = (
+            l_user_prompt 
+            + "\nHere's the function's locals variables which you can use as additional information to give your answer:\n"
+            + str(_function_locals)
+        )
+    
     response = model._api_call(
         sys_prompt=_emulator_pre_prompt,
         user_prompt=l_user_prompt,
