c1593bff3e63a548851228389b4cbb5e67a92311
remosleandre <leandre.ramos@epitech.eu>
2024-09-06 11:05:29 +0200

    [FIX] remove the #TODO

--- src/OpenHosta/OpenHosta.py
+++ src/OpenHosta/OpenHosta.py
@@ -9,8 +9,6 @@ DefaultManager.set_default_model(Model(
     base_url="https://api.openai.com/v1/chat/completions"
 ))
 
-
-# TODO make the if else for data cache here !
 emulate = HostaInjector(_exec_emulate)
 
 __all__ = "emulate", "thought",
