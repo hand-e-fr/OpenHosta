b0a82ddcdd49ac613a26d9bbd8f90b612c979cb1
Khyk <146129739+MerlinDEVILLARD@users.noreply.github.com>
2024-09-09 11:58:39 +0200

    [ADD] fixes & emulate and thought func test

--- src/OpenHosta/thought.py
+++ src/OpenHosta/thought.py
@@ -8,11 +8,17 @@ l_default = DefaultManager.get_default_model()
 _x = PromptMananger()
 _thought_sys_prompt = _x.get_prompt("thought")
 
+g_return_type = ""
+
 def thought(key):
+    global g_return_type
     _function_infos = {
         "function_def": "",
         "function_call": "",
         "return_type": None,
+        "return_type": None,
+        "ho_example": None,
+        "function_locals": None
     }    
     def inner_func(*args, **kwargs):
         global l_default, _thought_sys_prompt
@@ -24,7 +30,7 @@ def thought(key):
             + f"{args}\n"
         )
         
-        response = l_default._api_call(
+        response = l_default.api_call(
             sys_prompt=_thought_sys_prompt,
             user_prompt=l_user_prompt,
             creativity=0.5,
@@ -32,26 +38,25 @@ def thought(key):
         )
         
         data = response.json()
-        json_string = data["choices"][0]["message"]["content"]
+        g_return_type = data["choices"][0]["message"]["content"]
         
         typed = (
             str(args)
             + "\n"
             + "Here's the return type that must respect for your response. The python type is in the key \"type\" of this JSON schema:\n"
-            + json_string
+            + g_return_type
             + "\n"
         )
-        
-        print(typed)
         
         try:
             _function_infos["function_def"] = key
             _function_infos["function_call"] = typed
             result = _exec_emulate(_function_infos)
         except Exception as e:
-            sys.stderr.write(Exception)
+            sys.stderr.write(f"{e}")
             sys.stderr.write("[LMDA_ERROR]")
             result = None
         return result
 
+    setattr(inner_func, "_return_type", g_return_type)
     return inner_func
