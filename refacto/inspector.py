1fa20634520922bc79d1034778b100dee99ab7d7
Khyk <146129739+MerlinDEVILLARD@users.noreply.github.com>
2024-10-17 10:21:20 +0200

    [ADD] new refacto struct

--- refacto/inspector.py
+++ refacto/inspector.py
@@ -1,41 +1,20 @@
 from __future__ import annotations
 
-from typing import Callable, Tuple, Any, Optional, List, Union, Literal, Dict
-from pydantic import BaseModel
+from typing import Tuple, Callable, Optional, Dict, Any
 from types import FrameType, CodeType
 import inspect
 
-from OpenHosta.utils.errors import FrameError
+from .errors import FrameError
 
-HOSTAPATH = "./"
-
-class Func(BaseModel):
-    """
-    Func is a Pydantic model representing a function's metadata.
-    Useful for the executive functions and the post-processing.
-
-    Attributes:
-        f_def (str): Simple definition of the function, e.g., 'def func(a:int, b:str)->int:'
-        f_name (str): Name of the function, e.g., 'func'
-        f_call (str): Actual call of the function, e.g., 'func(1, 'salut')'
-        f_args (dict): Arguments of the function, e.g., {'a': 1, 'b': 'salut'}
-        f_type (object): desired type of the input and output of the function
-    """
-    f_def: str
-    f_name: str
-    f_call: str
-    f_args: dict
-    f_type: Tuple[List[object], object]
-    f_locals:dict
+all = (
+    "HostaInspector"
+)
 
 class HostaInspector:
     """
     This class is the parent class for a lot of OpenHosta functionnality.
     It provides methods which are used in many cases.
     """
-    def __init__(self):
-        """ Initialize the HostaInspector instance """
-        pass
     
     @staticmethod
     def _extend(*, back_level:int=2)->Tuple[Callable, FrameType]:
@@ -174,154 +153,3 @@ class HostaInspector:
             attr_parser(obj.__func__, attr)
             return True
         raise AttributeError(f"[HostaInspector._attach] Failed to attach attributs. Object's type not supported: {type(obj)}.")
-
-# module abc avec un héritage abstrait
-class FuncAnalizer:
-    
-    def __init__(self, func: Callable):
-        pass
-    
-      
-class HostaInjector(HostaInspector):
-    """
-    Takes a function as a parameter and injects the filled 'Func' pydantic
-    as parameter.
-    All instances of this class must therefore have the pydantic 'Func' 
-    as the first parameter.
-    """
-    def __init__(self, exec:Callable):
-        """ Initialize the HostaInjector instance """
-        super().__init__()
-        self.exec = exec
-        self.infos = Func()
-        self.func = self._extend()
-
-    def __call__(self, *args, **kwargs)->Func:
-        """ 
-        Make the instance callable. 
-        Executes “self.exec”, providing the necessary information in a pydantic 'Func'.
-        """
-        self.infos = self._get_infos_func()
-        return self.exec(self.infos, args, kwargs)
-    
-    def _get_infos_func(self) -> None:
-        """
-        parse a function to handle some infos
-        """
-        self.infos.f_def, _    = self.func_def() # car defintion + proto !
-        self.infos.f_name   = self.func.__name__ # un truc comme ca a verifier meme enlever peut etre
-        self.infos.f_call   = self.func_call()
-        self.infos.f_args   = self.func_args()
-        self.infos.f_type   = self.func_type()
-        self.infos.f_locals = self.func_locals()
-    
-
-    # maybe put all of this func in another file for hosta injector clarity ?
-    
-    def func_def(self):
-        """
-        function that get the definition of his own func blablabal
-        """
-        if self.func == None or type(self.func) != Callable :
-            raise ValueError("blablabla faire sa propre erreur")
-        
-        sig = inspect.signature(self.func)
-
-        func_name = self.func.__name__
-        func_params = ", ".join(
-            [
-                (
-                    f"{param_name}: {param.annotation.__name__}"
-                    if param.annotation != inspect.Parameter.empty
-                    else param_name
-                )
-                for param_name, param in sig.parameters.items()
-            ]
-        )
-        func_return = (
-            f" -> {sig.return_annotation.__name__}"
-            if sig.return_annotation != inspect.Signature.empty
-            else ""
-        )
-        definition = (
-            f"```python\ndef {func_name}({func_params}):{func_return}\n"
-            f"    \"\"\"\n\t{self.func.__doc__}\n    \"\"\"\n```"
-        )
-        prototype = f"def {func_name}({func_params}):{func_return}"
-        return definition, prototype # on fait def + proto mais est ce que on en ferait pas qu'un sur les deux (juste à rajouter func.doc pour la def c'est pour ça stp merlin dit oui !!!! (et jovilait aussi)) 
-    def func_call(self):
-        pass
-    def func_args(self):
-        pass
-    def func_type(self):
-        pass
-    
-    def func_locals(self):
-        pass
-
-class HostaChecker(HostaInspector):
-    """
-    Post processing in the execution chain. 
-    Detect errors in the output to reduce uncertainty.
-    Manage all the typing process
-    """
-    def __init__(self):
-        """ Initialize the HostaInjector instance """
-        super().__init__()
-
-    def _type(self):
-        """ Checks output typing """
-        pass
-
-class MemoryNode(BaseModel):
-    """ All the data given by [example, thought, use] in the body of a function """
-    key:Literal["ex", "cot", "use"]
-    id:int
-    value:Union[str, dict, Callable]
-
-class HostaMemory(HostaInspector):
-    """
-    Handle all the data given in a body of a function and in the inspection,
-    sort of a temporary cache.
-    """
-    
-    def __init__(self):
-        """ Initialize the HostaMemory instance """
-        super().__init__()
-        self._data:List[MemoryNode] = []
-        self._last_added:MemoryNode = None
-        self._func:Func = None
-        
-    def __str__(self):
-        """
-        Manages a function's “_examples” and “_cothought” attributes,
-        printing each type of element in a specific syntax
-        """
-        pass
-    
-    @property
-    def data(self)->List[MemoryNode]:
-        return self.data
-    
-    @data.setter
-    def data(self, value):
-        """ Check for inconsistencies in the data order. """
-        pass
-        
-    def add(self):
-        """ Adds an element in the memory. """
-        pass
-        
-    def get(self):
-        """ Get all elements of a single type from memory. """
-        pass
-
-def set_hosta_path(path:str):
-    """ Set the path for the '__hostacache__' directory by changing HOSTAPATH constant """
-    pass
-
-def save_example(func:Callable):
-    """
-    Extends the life of function examples by saving them in a file external to the program.
-    """
-    pass
