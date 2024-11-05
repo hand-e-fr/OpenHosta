15971d0c51a57c69caa35e9c0066a185c2040e0a
WilliamJlvt <william.jolivet@epitech.eu>
2024-11-05 09:59:00 +0100

    [ADD] new model schema and builtins

--- src/OpenHosta/exec/predict/architecture/builtins/classification.py
+++ src/OpenHosta/exec/predict/architecture/builtins/classification.py
@@ -1,20 +1,31 @@
+from typing import Union
+
 from ..neural_network import NeuralNetwork, Layer, LayerType
 
 
 class ClassificationBuilder(NeuralNetwork):
-    def __init__(self):
+    def __init__(self,
+                 input_size: Union[int, None] = None,
+                 output_size: Union[int, None] = None,
+                 complexity: Union[float, None] = None,
+                 activation: LayerType = LayerType.RELU
+             ):
         super().__init__()
 
-    def architecture(self, input_size, output_size, config=None, complexity=1.0, activation=LayerType.RELU):
-        hidden_size_1 = int(input_size * (2 * complexity))
-        transition_size = int(((input_size * output_size) / 2) * complexity)
+        if input_size is None or output_size is None:
+            raise ValueError("Input and output size must be specified")
+
+        _complexity: float = complexity if complexity is not None else 1.0
+
+        hidden_size_1 = int(input_size * (2 * _complexity))
+        transition_size = int(((input_size * output_size) / 2) * _complexity)
 
         if input_size > output_size:
             hidden_size_2 = int(transition_size / output_size)
         else:
             hidden_size_2 = transition_size
 
-        hidden_size_3 = int(output_size * (2 * complexity))
+        hidden_size_3 = int(output_size * (2 * _complexity))
 
         self.add_layer(Layer(LayerType.LINEAR, in_features=input_size, out_features=hidden_size_1))
         self.add_layer(Layer(activation))
@@ -24,5 +35,3 @@ class ClassificationBuilder(NeuralNetwork):
         self.add_layer(Layer(activation))
         self.add_layer(Layer(LayerType.LINEAR, in_features=hidden_size_3, out_features=output_size))
         self.add_layer(Layer(LayerType.SOFTMAX))
-
-        return config
