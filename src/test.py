4ccb07ad5d496c5f5e91d5347fce2cb55a11bbd3
Khyk <146129739+MerlinDEVILLARD@users.noreply.github.com>
2024-11-07 11:56:26 +0100

    [FIX] pydantic usage in predict

---
+++ src/test.py
@@ -0,0 +1,15 @@
+from OpenHosta.OpenHosta import config, emulate
+
+config.set_default_apiKey("")
+
+def uwu(a: int, b: int) -> int:
+    """
+    This function returns the sum of two integers.
+    """
+    # example(a=48, b=42, hosta_out=90)
+    # example(a=1, b=2, hosta_out=3)
+    # example(a=2, b=1, hosta_out=3)
+    return emulate()
+
+
+print(uwu(1, 2))
