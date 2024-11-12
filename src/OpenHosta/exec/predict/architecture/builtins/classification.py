48dac7a9adaa26441f0f716a138dabc93f24e50d
WilliamJlvt <william.jolivet@epitech.eu>
2024-11-12 10:06:49 +0100

    [ADD] refacto

--- src/OpenHosta/exec/predict/architecture/builtins/classification.py
+++ src/OpenHosta/exec/predict/architecture/builtins/classification.py
@@ -1,45 +1,11 @@
+from abc import ABCMeta, ABC
 from typing import Optional
 
 from ..neural_network import NeuralNetwork, Layer, LayerType
+from ..base_architecture import BaseArchitecture
 from torch import nn
-# import torch.functional as F
 
-class ClassificationBuilder(NeuralNetwork):
-    def __init__(self,
-                 input_size: Optional[int] = None,
-                 output_size: Optional[int] = None,
-                 complexity: Optional[float] = None,
-                 activation: LayerType = LayerType.RELU
-             ):
-        super().__init__()
-
-        if input_size is None or output_size is None:
-            raise ValueError("Input and output size must be specified")
-
-        _complexity: float = complexity if complexity is not None else 1.0
-
-        hidden_size_1 = int(input_size * (2 * _complexity))
-        transition_size = int(((input_size * output_size) / 2) * _complexity)
-
-        if input_size > output_size:
-            hidden_size_2 = int(transition_size / output_size)
-        else:
-            hidden_size_2 = transition_size
-
-        hidden_size_3 = int(output_size * (2 * _complexity))
-
-        self.add_layer(Layer(LayerType.LINEAR, in_features=input_size, out_features=hidden_size_1))
-        self.add_layer(Layer(activation))
-        self.add_layer(Layer(LayerType.LINEAR, in_features=hidden_size_1, out_features=hidden_size_2))
-        self.add_layer(Layer(activation))
-        self.add_layer(Layer(LayerType.LINEAR, in_features=hidden_size_2, out_features=hidden_size_3))
-        self.add_layer(Layer(activation))
-        self.add_layer(Layer(LayerType.LINEAR, in_features=hidden_size_3, out_features=output_size))
-        self.add_layer(Layer(LayerType.SOFTMAX))
-
-
-
-class Classification(NeuralNetwork, nn.Module):
+class Classification(nn.Module, BaseArchitecture):
     def __init__(self, input_size : int, output_size : int, complexity : int):
         super().__init__()
 
@@ -77,11 +43,3 @@ class Classification(NeuralNetwork, nn.Module):
                 loss = self.loss(output, labels)
                 loss.backward()
                 self.optimizer.step()
-
-
-    
-
-    
-
-
-
