1bac445306cfb0bfa8f9f4eedddf9cd8967ca2e4
WilliamJlvt <william.jolivet@epitech.eu>
2024-11-13 13:30:05 +0100

    [ADD] refacto name

--- src/OpenHosta/exec/predict/model/model_provider.py
+++ src/OpenHosta/exec/predict/model/model_provider.py
@@ -5,6 +5,7 @@ from .builtins.classification import Classification
 from .builtins.linear_regression import LinearRegression
 from .hosta_model import HostaModel
 from .neural_network import NeuralNetwork
+from ..config_model import ConfigModel
 from ....utils.torch_nn_utils import type_size
 
 
@@ -17,7 +18,7 @@ class ArchitectureType(Enum):
 
 class HostaModelProvider:
     @staticmethod
-    def from_hosta_func(func, model, path, verbose):
+    def from_hosta_func(func, model: ConfigModel, path, verbose):
         input_size = type_size(func.f_type[0])
         output_size = type_size(func.f_type[1])
         architecture: Optional[HostaModel] = None
@@ -32,7 +33,7 @@ class HostaModelProvider:
             elif model.model_type == ArchitectureType.CLASSIFICATION:
                 architecture = Classification(cached_architecture, input_size, output_size, model.complexity)
         else:
-            raise ValueError("Tempo: Model type must be specified")
+            architecture = LinearRegression(cached_architecture, input_size, output_size, model.complexity)
 
         with open(path, 'w') as file:
             file.write(NeuralNetwork.from_torch_nn(architecture).to_json())
