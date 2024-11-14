eb07f108765287c9706b96e19271e4ccf02337df
WilliamJlvt <william.jolivet@epitech.eu>
2024-11-14 09:30:59 +0100

    [ADD] change name

--- src/OpenHosta/exec/predict/predict_config.py
+++ src/OpenHosta/exec/predict/predict_config.py
@@ -9,12 +9,13 @@ class PredictConfig:
                  name: str = None,
                  path: str = None,
                  version: str = None,
-                 complexity: Optional[float] = None,
+                 complexity: int = 4,
+                 max_tokens: int = 10,
                  epochs: Optional[int] = None,
                  batch_size: Optional[int] = None,
                  learning_rate: Optional[float] = None,
                  get_loss: Optional[float] = None,
-                 dataset_path: Optional[str] = None,
+                 dataset_path: Optional[str] = None
              ):
         self.model_type: ArchitectureType = model_type
 
@@ -22,7 +23,8 @@ class PredictConfig:
         self.path: str = path
         self.version: str = version
 
-        self.complexity: float = complexity
+        self.complexity: int = complexity
+        self.max_tokens: int = max_tokens
 
         self.batch_size: int = batch_size
         self.epochs: int = epochs
@@ -37,11 +39,12 @@ class PredictConfig:
             "weight_path": "{self.path}",
             "version": "{self.version}",
             "complexity": {self.complexity},
+            "max_tokens": {self.max_tokens},
             "epochs": {self.epochs},
             "batch_size": {self.batch_size},
             "learning_rate": {self.learning_rate},
             "get_loss": {self.get_loss},
-            "dataset_path": "{self.dataset_path}"
+            "dataset_path": "{self.dataset_path}",
         }}"""
 
     @staticmethod
@@ -54,26 +57,10 @@ class PredictConfig:
             path=data["path"],
             version=data["version"],
             complexity=data["complexity"],
+            max_tokens=data["max_tokens"],
             epochs=data["epochs"],
             batch_size=data["batch_size"],
             learning_rate=data["learning_rate"],
             get_loss=data["get_loss"],
-            dataset_path=data["dataset_path"]
-
+            dataset_path=data["dataset_path"],
         )
-    
-
-
-
-class New_Config_Model:
-    archi_type: ArchitectureType
-    archi_complexity: int
-    archi_name: str
-    archi_version: float
-    train_epochs: int
-    train_batch_size: int
-    train_learning_rate: float
-    train_get_loss: float
-    data_path: str
-    data_validation: bool # val_set or no
-    
