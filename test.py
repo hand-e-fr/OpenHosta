b31268f764b3dcf174eca11e4454b1d733a651c7
Khyk <146129739+MerlinDEVILLARD@users.noreply.github.com>
2024-11-19 08:08:56 +0100

    [FIX] realeases bug

---
+++ test.py
@@ -0,0 +1,9 @@
+from OpenHosta import emulate
+
+def multiply(a:int, b:int)->int:
+    """
+    This function multiplies two integers in parameter.
+    """
+    return emulate()
+
+print(emulate(5, 6))
