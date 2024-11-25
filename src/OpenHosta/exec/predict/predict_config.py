3e67c260266b86dc422dea186886ee8777443e54
remosleandre <leandre.ramos@epitech.eu>
2024-11-25 14:38:58 +0100

    [FIX] predict_config and add logger in predict

--- src/OpenHosta/exec/predict/predict_config.py
+++ src/OpenHosta/exec/predict/predict_config.py
@@ -5,47 +5,32 @@ from .model.neural_network_types import ArchitectureType
 
 class PredictConfig:
     def __init__(self,
-                 model_type: ArchitectureType = None,
                  name: str = None,
                  path: str = None,
-                 version: str = None,
-                 complexity: int = 4,
-                 max_tokens: int = 10,
+                 complexity: int = 5,
                  epochs: Optional[int] = None,
                  batch_size: Optional[int] = None,
-                 learning_rate: Optional[float] = None,
-                 get_loss: Optional[float] = None,
+                 max_tokens: int = 1,
                  dataset_path: Optional[str] = None,
-                 generated_data: Optional[int] = 200,
-             ):
-        self.model_type: ArchitectureType = model_type
-
+                 generated_data: Optional[int] = 100,
+                ):
         self.name: str = name
         self.path: str = path
-        self.version: str = version
-
         self.complexity: int = complexity
-        self.max_tokens: int = max_tokens
-
-        self.batch_size: int = batch_size
         self.epochs: int = epochs
-        self.learning_rate: float = learning_rate
-        self.get_loss: float = get_loss
+        self.batch_size: int = batch_size
+        self.max_tokens: int = max_tokens
         self.dataset_path: str = dataset_path
         self.generated_data: int = generated_data
 
     def to_json(self):
         return f"""{{
             "name": "{self.name}",
-            "model_type": "{self.model_type}",
-            "weight_path": "{self.path}",
-            "version": "{self.version}",
+            "path": "{self.path}",
             "complexity": {self.complexity},
-            "max_tokens": {self.max_tokens},
             "epochs": {self.epochs},
             "batch_size": {self.batch_size},
-            "learning_rate": {self.learning_rate},
-            "get_loss": {self.get_loss},
+            "max_tokens": {self.max_tokens},
             "dataset_path": "{self.dataset_path}",
             "generated_data": {self.generated_data}
         }}"""
@@ -56,15 +41,11 @@ class PredictConfig:
         data = json.loads(json_str)
         return PredictConfig(
             name=data["name"],
-            model_type=ArchitectureType(data["model_type"]),
             path=data["path"],
-            version=data["version"],
             complexity=data["complexity"],
-            max_tokens=data["max_tokens"],
             epochs=data["epochs"],
             batch_size=data["batch_size"],
-            learning_rate=data["learning_rate"],
-            get_loss=data["get_loss"],
+            max_tokens=data["max_tokens"],
             dataset_path=data["dataset_path"],
             generated_data=data["generated_data"]
         )
