4b0ce0c97054745a5d8146b46209fde3d7fcff95
remosleandre <leandre.ramos@epitech.eu>
2024-12-17 14:31:41 +0100

    cuda another fix

--- src/OpenHosta/exec/predict/model/hosta_model.py
+++ src/OpenHosta/exec/predict/model/hosta_model.py
@@ -7,7 +7,7 @@ from .neural_network_types import ArchitectureType
 
 
 class HostaModel(ABC, nn.Module):
-    def __init__(self, device: Optional[str]):
+    def __init__(self):
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.layers = []
         self.architecture_type: Optional[ArchitectureType] = None
