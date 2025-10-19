3241958f52f6cd3436504ef0e05babef03583338
Emmanuel BATT <ebatt@hand-e.fr>
2025-10-19 13:41:37 +0200

    add OCR test and load assets from repo

--- tests/functionnal/test_multimodal.py
+++ tests/functionnal/test_multimodal.py
@@ -1,14 +1,27 @@
-from OpenHosta.core.logger import print_last_prompt
 import pytest
 
+
+from PIL.Image import Image, open
+from pathlib import Path
+
+from typing import List
+
+try:
+    test_dir = Path(__file__).parent.parent
+except NameError:
+    print("In interactive mode, setting test_dir to current working directory. It shall be the tests folder.")
+    test_dir = Path.cwd()
+    
+if (test_dir / "assets").exists():
+    assets_dir = test_dir / "assets"
+else:
+    raise FileNotFoundError(f"Assets directory not found in {test_dir}. Cannot continue.")
+    
 from OpenHosta import emulate
 
 class TestMultiModal:
        
     def test_ImageInPrompt(self):
-        from PIL.Image import Image, open
-        from io import BytesIO
-        import requests
         
         def WhatCompanyLogo(logo:Image) -> str:
             """
@@ -16,8 +29,53 @@ class TestMultiModal:
             """
             return emulate()
         
-        # Load from URL
-        url = "https://www.hand-e.fr/hand_e_logo.jpeg"
-        img = open(BytesIO(requests.get(url).content))
+        img = open(assets_dir / "hand_e_logo.jpeg")
         response = WhatCompanyLogo(img)
-        assert "and" in response, "We should have recognized hand-e logo"
+        assert "hand" in response.lower(), "We should have recognized hand-e logo"
+
+    def test_ReadTableInImage(self):
+
+        expected_lines = [
+             'GUNOOLIONCSEHP',
+             'AHWZMEREFAEVOS',
+             'SOUSLMARINAPPI',
+             'VYYCAMIONEAGGH',
+             'RENARDAMIBILAU',
+             'YEGICHÂTEAUBAD',
+             'VOITUREIXRHYNV',
+             'THLEYLDROBOTGV',
+             'QAADSLIVREDPBK',
+             'UTLTGRPDIFUSÉE',
+             'ILUCHIENBLOUPE',
+             'POULESOURSONTL',
+             'PIRATECMUILOLM', 
+             'CULBUTOIOPOINTS'
+            ]
+        
+        def image_of_char_grid_to_list_of_string(image: Image) -> List[str]:
+            """
+            Convert an image of a character grid into a list of strings.
+            Each string represents a row of characters.
+            Rows does not have meaning. Just extract the characters as they are in the image.
+            
+            Arguments:
+                image (PIL.Image.Image): The input image containing the character grid.
+                
+            Returns:
+                List[str]: A list of strings. each string is a line of the grid. no space in between.
+            """
+            return emulate()
+
+        img = open(assets_dir / "letter_grid.png")
+
+        lines = image_of_char_grid_to_list_of_string(img)
+
+        assert type(lines) == list, "The output should be a list of strings"
+        assert len(lines) > 0, "We should have extracted some lines from the image"
+        assert len(lines) == len(expected_lines), f"We should have extracted {len(expected_lines)} lines"
+        
+        for line in lines:
+            assert type(line) == str, "Each line should be a string"
+
+        for i, expected_line in enumerate(expected_lines):
+            assert expected_line == lines[i], f"The line {i} should be '{expected_line}' but got '{lines[i]}'"
