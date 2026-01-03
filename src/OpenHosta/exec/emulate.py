2f81bb30a91a578c8b6ca5b29d6216f23ebc0219
Emmanuel BATT <ebatt@hand-e.fr>
2026-01-04 00:12:31 +0100

    add emulate_iterator draft

--- src/OpenHosta/exec/emulate.py
+++ src/OpenHosta/exec/emulate.py
@@ -81,3 +81,78 @@ async def emulate_async(
     
     return response_data
 
+from ..core.uncertainty import safe
+import math
+
+def emulate_iterator(
+    pipeline : OneTurnConversationPipeline = config.DefaultPipeline,
+    max_generation = 100,
+    min_probability = 1e-8,
+    assistant_thinking_string = "",
+    force_llm_args: Optional[dict] = {}):
+
+    # You can retrive this frame using get_last_frame(your_emulated_function) in interactive mode
+    frame = get_caller_frame()
+
+    # Get everything about the function you are emulating
+    inspection = get_hosta_inspection(frame)
+
+    def _iterator():
+        
+        ## TODO: top_k = 1 pour génération avec le toplogprob n°1
+        ## Check for Unertainty error. if raised, continue 
+        ## - Yield avec (cumulated_prob, answer)
+        ## - split à chaque fois qu'il y a des logprobes > average logprob
+        ## - tri des branchements par proba
+        ## - génération de la branche avec la plus haute probabilité (ajout dans messages pour assistant)
+        ## !! si top_logprob[20] > average_logprob alors il faut raise un erreur qui indique que la génération est incomplete. Il faut ajouter une catégorie.
+        ## !! que faut il faire si le toplogprob n+1 est un substring d'un toplogprob [0..n] ? => générer la branche complete et regarder si le segement suivant (jusqu'au branchage suivant) est est identique en str. si oui: merge de la proba avec le précédent, sinon branche accepté 
+        
+        ## TODO: test si les prob sont similaires lorsque l'ordre des catégories est inversé
+        ## 
+        
+        with safe(acceptable_cumulated_uncertainty=100) as safe_context:
+            # Convert the inspection to a prompt
+            messages = pipeline.push(inspection)
+            messages.append({"role":"assistant", "content":[]})
+            response_dict = inspection.model.api_call(messages, inspection.force_llm_args | force_llm_args)
+            response_data = pipeline.pull(inspection, response_dict)
+            yield response_data
+            
+            response_logprobs = inspection.logs["llm_api_response"]["choices"][0]["logprobs"]["content"]
+            
+            vocabulary_size = 200000
+            path = "'"
+            for i, token_data in enumerate(response_logprobs):
+                
+                top_logprobs = token_data.get("top_logprobs", [])
+                likely_alternatives = [
+                    p for p in top_logprobs
+                    if p.get('logprob', -float('inf')) > math.log(1 / vocabulary_size)
+                ]
+
+                print("I:", i, likely_alternatives)
+                
+                for token in likely_alternatives:
+                    
+                    print(token)
+                    start = path + token["token"]
+                    print(start)
+                    messages[-1]["content"] = [{"type":"text", "text":assistant_thinking_string + start}]
+
+                    response_dict = inspection.model.api_call(messages, inspection.force_llm_args | force_llm_args)
+                    
+                    # Most likely due to the fact that gpt-4.1 has very low probs for single letters tokens
+                    answer = response_dict["choices"][0]["message"]["content"]
+                    response_dict["choices"][0]["message"]["content"] = assistant_thinking_string + start + answer
+
+                    # Convert the model response to a python object according to expected types
+                    response_data = pipeline.pull(inspection, response_dict)
+                    
+                    print(safe_context.acceptable_cumulated_uncertainty)
+                    
+                    yield response_data
+                    
+                path += token_data['token']
+
+    return _iterator()
--- tests/functionnal/test_generate.py
+++ tests/functionnal/test_generate.py
@@ -5,91 +5,59 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+from OpenHosta import emulate_iterator
 
-
-from OpenHosta import emulate, safe
-#from OpenHosta import emulate_iterator
-from OpenHosta import OneTurnConversationPipeline, config
-
-
-from OpenHosta.core.inspection import get_caller_frame, get_hosta_inspection
-from OpenHosta.core.config import config
-from OpenHosta.pipelines import OneTurnConversationPipeline
-
-def emulate_iterator(pipeline : OneTurnConversationPipeline = config.DefaultPipeline, max_generation = 100, min_probability = 1e-8, *args, **kwargs):
-
-    # You can retrive this frame using get_last_frame(your_emulated_function) in interactive mode
-    frame = get_caller_frame()
-
-    # Get everything about the function you are emulating
-    inspection = get_hosta_inspection(frame)
-
-    def _wrapper():
-        
-        ## TODO: top_k = 1 pour génération avec le toplogprob n°1
-        ## Check for Unertainty error. if raised, continue 
-        ## - Yield avec (cumulated_prob, answer)
-        ## - split à chaque fois qu'il y a des logprobes > average logprob
-        ## - tri des branchements par proba
-        ## - génération de la branche avec la plus haute probabilité (ajout dans messages pour assistant)
-        ## !! si top_logprob[20] > average_logprob alors il faut raise un erreur qui indique que la génération est incomplete. Il faut ajouter une catégorie.
-        ## !! que faut il faire si le toplogprob n+1 est un substring d'un toplogprob [0..n] ? => générer la branche complete et regarder si le segement suivant (jusqu'au branchage suivant) est est identique en str. si oui: merge de la proba avec le précédent, sinon branche accepté 
-        
-        ## TODO: test si les prob sont similaires lorsque l'ordre des catégories est inversé
-        ## 
-                
-        # Convert the inspection to a prompt
-        messages = pipeline.push(inspection)
-        
-        try:
-            # This is the api call to the model, nothing more. Easy to debug and test.
-            response_dict = inspection.model.api_call(messages, inspection["force_llm_args"])
-        except RateLimitError as e:
-            try:
-                retry_delay = int(os.getenv("OPENHOSTA_RATE_LIMIT_WAIT_TIME", 60))
-            except:
-                retry_delay = 0
-
-            print(f"[emulate] Rate limit exceeded. We wait for {retry_delay}s then retry.", e)
-
-            if retry_delay == 0:
-                raise e
-            else:
-                time.sleep(retry_delay)
-                response_dict = inspection["model"].api_call(messages, inspection["force_llm_args"])
-            
-        # Convert the model response to a python object according to expected types
-        response_data = pipeline.pull(inspection, response_dict)
-        
-        return response_data
-
-
-
-
-    return _wrapper()
-
+# Force logprobs capability for testing
+from OpenHosta import config
+from OpenHosta.core.base_model import ModelCapabilities
+config.DefaultModel.capabilities |= {ModelCapabilities.LOGPROBS}
 
 # Basic test to check if the emulate function works with a simple prompt
 def test_generate_basic():
     """
+    This test checks if the emulate function works with a simple prompt in iterator mode.
     """
-    
+    from OpenHosta import print_last_prompt
+    assistant_thinking_string = """Here is a random answer in asia """
+    assistant_thinking_string = """<think>
+    This time let's consider the answer that starts with '"""
     def country_name() -> str:
         """
         This function returns the name of a country.
 
         Returns:
-            str: The name of a county.
+            str: The name of a country.
         """
-        return emulate_iterator()
+        return emulate_iterator(max_generation=10, assistant_thinking_string=assistant_thinking_string)
 
-    country_name()    
+    it = country_name()
+    next(it)
+    for c in country_name():
+        print(c)
+        break
+    print_last_prompt(country_name)
+
+    def letters_of_the_alphabet() -> str:
+        """
+        This function returns a random letter of the alphabet.
+        It should return only one character.
+
+        Returns:
+            str: The letter.
+        """
+        return emulate_iterator(max_generation=4, assistant_thinking_string=assistant_thinking_string)
+
+    for c in letters_of_the_alphabet():
+        print(c)
+        break
+        
+    print_last_prompt(letters_of_the_alphabet)
     
-    def get_capital(country: str) -> str:
+    def get_city(country: str) -> str:
         """
         This function returns the capital of a given country.
         """
-        return emulate()
-    
-    response = get_capital("France")
-    assert "Paris" in response, f"Expected 'Paris' in response, got: {response}"
+        return emulate_iterator(max_generation=5,assistant_thinking_string=assistant_thinking_string)
+
+    for c in get_city("France"):
+        print(c)
