3e76debc1126f4000cbc237eae4b65e1dd0991d6
BATT Emmanuel-EXT <emmanuel.batt-ext@alstomgroup.com>
2026-03-22 23:21:07 +0100

    cicd: make dotenv optional and restore test in git action

--- tests/functionnal/test_ask.py
+++ tests/functionnal/test_ask.py
@@ -1,9 +1,5 @@
-import os
 import time
 import pytest
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # To run these tests you need to set .env variables to define an LLM provider
 # e.g. for OpenAI:
