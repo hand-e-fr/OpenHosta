658313cc5bf6252294b1ac36665c5d32b48f83d8
BATT Emmanuel-EXT <emmanuel.batt-ext@alstomgroup.com>
2026-04-24 18:36:39 +0200

    refacto: Unification des blocs de code dans le template.

--- src/OpenHosta/core/meta_prompt.py
+++ src/OpenHosta/core/meta_prompt.py
@@ -109,11 +109,11 @@ EMULATE_META_PROMPT=MetaPrompt(
     explain it in between <think></think> tags.{% endif %}
 
     Here's the function definition:
-
-    {{ python_type_definition_dict }}
     
     ```python    
-    def {{ function_name }}({{ function_args }}) -> {{ function_return_type_name }}:
+    {% if python_type_definition_dict %}{{ python_type_definition_dict }}
+    
+    {% endif %}def {{ function_name }}({{ function_args }}) -> {{ function_return_type_name }}:
         \"\"\"
         {{ function_doc | indent(4, true) }}
         \"\"\"
